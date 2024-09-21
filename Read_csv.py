cf8ec625df68a7ee7397939e0ac876fc963bbe69
MilindKeer <117166688+MilindKeer@users.noreply.github.com>
2024-09-21 08:53:09 +0000

    uploded paqquet file

--- Read_csv.py
+++ Read_csv.py
@@ -2,8 +2,18 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("temp").getOrCreate()
 
-df = spark.read.csv("data/recs2015_public_v4.csv", header=True, inferSchema=True)
+df = spark.read.csv("/workspaces/Test/data/people.csv", header=True, inferSchema=True)
+df.show(20)
+df.printSchema()
+df.select('id','name','age').show()
 
-df.show()
+df_clean = df.na.drop()
+df_clean.show()
+
+df.createOrReplaceTempView("people")
+
+df_clean = spark.sql("Select * from people where age is not null")
+df_clean.show()
 
 spark.stop
+
